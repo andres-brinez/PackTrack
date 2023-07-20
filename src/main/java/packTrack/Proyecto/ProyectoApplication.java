12e5133f714b987cc66a0eb57a7c8c2ed08d01ce
andres briñez <brinezlopez08@gmail.com>
2023-07-20 13:26:26 -0500

    Crear las  primeras pruebas para el uso del modelo

--- src/main/java/packTrack/Proyecto/ProyectoApplication.java
+++ src/main/java/packTrack/Proyecto/ProyectoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packTrack.Proyecto.modelos.Usuario;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // indica que es una aplicacion de spring boot
 @RestController //  marca la clase como un controlador de tipo REST para manejar las solicitudes HTTP y enviar respuestas
@@ -12,12 +13,27 @@ public class ProyectoApplication {
 
     public static void main(String[] args) {SpringApplication.run(ProyectoApplication.class, args);}// Inicia la aplicacion como punto de partida
 
-    
+	// * Informacion
+	// ? exito
+	// ! error
+	// TODO: pendiente
+
+
+    //? SERVICIOS REST
 	@GetMapping("/inicio") // indica que el metodo maneja las solicitudes HTTP GET
 	public String hello() {
-		return "Hola Mundo";
+		return "Hola a todAs ";
 	}
 
+	@GetMapping("/pruebaModelo")
+	public String prueba() {
+
+		Usuario usuario = new Usuario(1105610650,"Andres Felipe Briñez","comedor20","Administrador");
+		usuario.setNombre("Maria Lopez");
+		System.out.println(usuario.getNombre());
+		return usuario.getNombre();
+
+	}
 
 
 }
