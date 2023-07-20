fa475123e6ea770e460cf99bada69358b7419f58
andres briñez <brinezlopez08@gmail.com>
2023-07-20 10:19:17 -0500

    Se crea el hola mundo

--- src/main/java/packTrack/Proyecto/ProyectoApplication.java
+++ src/main/java/packTrack/Proyecto/ProyectoApplication.java
@@ -2,12 +2,22 @@ package packTrack.Proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // indica que es una aplicacion de spring boot
+@RestController //  marca la clase como un controlador de tipo REST para manejar las solicitudes HTTP y enviar respuestas
 public class ProyectoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(ProyectoApplication.class, args);}// Inicia la aplicacion como punto de partida
+
+    
+	@GetMapping("/inicio") // indica que el metodo maneja las solicitudes HTTP GET
+	public String hello() {
+		return "Hola Mundo";
 	}
+
+
 
 }
