197482f8356a5fb040c38856fdef29d39641a96c
andres briñez <brinezlopez08@gmail.com>
2023-07-21 16:56:41 -0500

    Quitar controladores

--- src/main/java/packTrack/Proyecto/ProyectoApplication.java
+++ src/main/java/packTrack/Proyecto/ProyectoApplication.java
@@ -3,12 +3,8 @@ package packTrack.Proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import packTrack.Proyecto.modelos.Envio;
-import packTrack.Proyecto.modelos.Factura;
-import packTrack.Proyecto.modelos.Paquete;
-import packTrack.Proyecto.modelos.Usuario;
+
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // indica que es una aplicacion de spring boot
 @RestController //  marca la clase como un controlador de tipo REST para manejar las solicitudes HTTP y enviar respuestas
@@ -22,50 +18,9 @@ public class ProyectoApplication {
 	// TODO: pendiente
 
 
-    //? SERVICIOS REST
-	@GetMapping("/inicio") // indica que el metodo maneja las solicitudes HTTP GET
-	public String hello() {
-		return "Hola a todAs ";
-	}
 
-	@GetMapping("/pruebaModeloUsuario")
-	public String prueba() {
 
-		Usuario usuario = new Usuario(1105610650,"Andres Felipe Briñez","comedor20","Administrador");
-		usuario.setNombre("Maria Lopez");
-		System.out.println(usuario.getNombre());
-		return usuario.getNombre();
 
-	}
-
-	@GetMapping("/pruebaModeloPaquete")
-	public String pruebaPaquete() {
-		Usuario empleado = new Usuario(1105610650,"Andres Felipe Briñez","comedor20","Empleado");
-		Usuario usuario= new Usuario(1125646650,"Juan Felipe Lopez","comedor20","Usuario");
-
-		Paquete paquete = new Paquete(usuario,empleado,"Paquete 1","Bogota","Paquete de prueba",1,200,100,200,300,100000);
-		return paquete.toString();
-	}
-
-    @GetMapping("/pruebaModeloEnvio")
-    public String pruebaEnvio() {
-        Usuario empleado = new Usuario(1105610650,"Andres Felipe Briñez","comedor20","Empleado");
-        Usuario usuario= new Usuario(1125646650,"Juan Felipe Lopez","comedor20","Usuario");
-        Paquete paquete = new Paquete(usuario,empleado,"Paquete 1","Bogota","Paquete de prueba",1,200,100,200,300,100000);
-        Envio envio = new Envio(paquete,"Bogota","Maria Lopez","Envio de un paquete","Economico");
-        return envio.toString();
-    }
-
-	@GetMapping("/pruebaModeloFactura")
-	public String pruebaFactura() {
-		Usuario empleado = new Usuario(1105610650,"Andres Felipe Briñez","comedor20","Empleado");
-		Usuario usuario= new Usuario(1125646650,"Juan Felipe Lopez","comedor20","Usuario");
-		Paquete paquete = new Paquete(usuario,empleado,"Paquete 1","Bogota","Paquete de prueba",1,200,100,200,300,100000);
-		Envio envio = new Envio(paquete,"Bogota","Maria Lopez","Envio de un paquete","Economico");
-		Factura factura = new Factura(envio);
-
-		return factura.generarFactura();
-	}
 
 
 
