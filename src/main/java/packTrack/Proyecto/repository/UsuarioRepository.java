183950c1b2b19b5c0db4f1b121c362b81ea23c51
andres briñez <brinezlopez08@gmail.com>
2023-07-21 14:23:29 -0500

    Crear el repositorio

---
+++ src/main/java/packTrack/Proyecto/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package packTrack.Proyecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import packTrack.Proyecto.modelos.Usuario;
+
+@Repository                                // <Clase relacionada, Tipo de dato de la llave primaria>
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+}
