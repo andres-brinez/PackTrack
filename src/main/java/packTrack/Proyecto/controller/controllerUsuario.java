e8e2f15077abd0526208c053927dd555b42146bd
andres briñez <brinezlopez08@gmail.com>
2023-07-21 23:56:20 -0500

    Actualizar rutas y crear servicio para actualizar usuario

--- src/main/java/packTrack/Proyecto/controller/controllerUsuario.java
+++ src/main/java/packTrack/Proyecto/controller/controllerUsuario.java
@@ -19,18 +19,18 @@ public class controllerUsuario {
 
     //? SERVICIOS REST
 
-    @GetMapping({"/","/HistorialUsuarios"}) // {ruta1, ruta2,..} maneja varias rutas para el mismo metodo (servicio)
+    @GetMapping({"/","/historialUsuarios"}) // {ruta1, ruta2,..} maneja varias rutas para el mismo metodo (servicio)
     public String viewUsuarios(Model model) {
         List<Usuario> listaUsuarios = usuarioService.getAllUsuarios(); // Se obtiene la lista de usuarios usando el metodo del servicio que deuelve la lista de usuarios
         model.addAttribute("listaUsuarios", listaUsuarios); // Se agrega la lista de usuarios al modelo para poder usarla en la vista
         return "usuarios/historialUsuarios"; // Se retorna el nombre de la vista
     }
 
-    @GetMapping("/usuarios/crearUsuario")
+    @GetMapping("/crearUsuario")
     public String crearUsuario(Model model) {
         Usuario nuevoUsuario = new Usuario();
         model.addAttribute("usuario", nuevoUsuario); //se guarda un objeto en el  modelo para poder usarlo en la vista y guardar valores
-        return "crearUsuario"; // Se retorna el nombre de la vista
+        return "/usuarios/crearUsuario"; // Se retorna el nombre de la vista
     }
 
     // Para guardar el usuario se usa el metodo del servicio que guarda el usuario en la base de datos
@@ -39,19 +39,30 @@ public class controllerUsuario {
 
         if (usuarioService.saveOrUpdateUsuario(usuario)==true) { // Si el usuario se guarda correctamente
             //redirectAttributes.addFlashAttribute("success", "Usuario guardado correctamente"); // Se agrega un atributo al modelo para poder usarlo en la vista
-            return "redirect:/usuarios"; // Se redirecciona al servicio, no al template
+            return "redirect:/historialUsuarios"; // Se redirecciona al servicio, no al template
 
         }
             //redirectAttributes.addFlashAttribute("error", "No se pudo guardar el usuario"); // Se agrega un atributo al modelo para poder usarlo en la vista
         return "redirect:/crearUsuario";   // Se redirecciona al servicio, no al template
     }
 
-    @GetMapping("/editarUsuario")
+    @GetMapping("/editarUsuario/{numeroIdentificacion}")
     //@PathVariable se usa para obtener el valor de la variable en la ruta
     public String editarUsuario(Model model, @PathVariable long numeroIdentificacion) {
         Usuario usuario = usuarioService.getUsuarioById(numeroIdentificacion);
         model.addAttribute("usuario", usuario);
         return "usuarios/editarUsuario";
+    }
+
+    @PostMapping("/actualizarUsuario")
+    public String actualizarUsuario(Usuario usuario) {
+        System.out.println(usuario.getNumeroIdentificacion());
+        if (usuarioService.saveOrUpdateUsuario(usuario)){
+            //redirectAttributes.addFlashAttribute("success", "Usuario actualizado correctamente");
+            return "redirect:/historialUsuarios";
+        }
+        //redirectAttributes.addFlashAttribute("error", "No se pudo actualizar el usuario");
+        return "redirect:/editarUsuario/" + usuario.getNumeroIdentificacion();
     }
 
 
