4972b1f53431dd191d4d5c8f7a699594780e7a9c
andres briñez <brinezlopez08@gmail.com>
2023-08-11 22:49:47 -0500

    ..

--- src/main/java/packTrack/Proyecto/ProyectoApplication.java
+++ src/main/java/packTrack/Proyecto/ProyectoApplication.java
@@ -3,10 +3,9 @@ package packTrack.Proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // indica que es una aplicacion de spring boot
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // indica que es una aplicacion de spring boot y que se excluya la configuracion de seguridad
 public class ProyectoApplication {
 
     public static void main(String[] args) {SpringApplication.run(ProyectoApplication.class, args);}// Inicia la aplicacion como punto de partida
