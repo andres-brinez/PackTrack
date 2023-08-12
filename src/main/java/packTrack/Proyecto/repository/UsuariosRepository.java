e66eb81fd50b4ddcca3fc37af7b972b23ac1476c
andres briñez <brinezlopez08@gmail.com>
2023-08-11 19:57:19 -0500

    Permitir buscar un usuario por rol

--- src/main/java/packTrack/Proyecto/repository/UsuariosRepository.java
+++ src/main/java/packTrack/Proyecto/repository/UsuariosRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import packTrack.Proyecto.modelos.Usuario;
 
+import java.util.List;
+
 @Repository                                // <Clase relacionada, Tipo de dato de la llave primaria>
 public interface UsuariosRepository extends JpaRepository <Usuario, Long> {
+
+    // obtener los usuarios que son Empleados
+    List<Usuario> findByRol(String rol);
 }
