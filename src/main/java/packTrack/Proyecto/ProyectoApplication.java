1d0486b42db9baf54a9c735d1317b47d60e83efa
andres briñez <brinezlopez08@gmail.com>
2023-08-17 00:07:08 -0500

    Excluir la seguridad

--- src/main/java/packTrack/Proyecto/ProyectoApplication.java
+++ src/main/java/packTrack/Proyecto/ProyectoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 
 @SpringBootApplication() // indica que es una aplicacion de spring boot
+// exclude={SecurityAutoConfiguration.class}  deshabilita la configuracion de seguridad automatica de spring boot
+// para poder usar la configuracion de seguridad personalizada
+
 public class ProyectoApplication {
 
     public static void main(String[] args) {SpringApplication.run(ProyectoApplication.class, args);}// Inicia la aplicacion como punto de partida
